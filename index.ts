228d51c3803bdafab2abb9ac0c819982ec040c54
Storisi <stiivenmendez16@gmail.com>
2024-02-26 14:05:22 -0500

    services

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export * from './services/get-form-fields.service'
+export * from './services/get-requests.service'
+export * from './services/get-token.service'
+export * from './services/create-request.service'
---
+++ interceptors/axios.interceptor.ts
@@ -0,0 +1,31 @@
+import axios, { InternalAxiosRequestConfig } from 'axios'
+import getToken from '../services/get-token.service'
+
+const updateHeader = async (request: InternalAxiosRequestConfig<any>) => {
+  request.headers['Content-Type'] = 'application/json'
+
+  const token = await getToken({
+    username: 'admin',
+    password: 'admin',
+  })
+  request.headers['Authorization'] = `Bearer ${token}`
+  return request
+}
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:5150/sammapi',
+})
+
+axiosInstance.interceptors.request.use((request) => {
+  return updateHeader(request)
+})
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+export default axiosInstance
